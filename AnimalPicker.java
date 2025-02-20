a608e153d7f38c007cd5c25833c6c710e37dd762
isabeleila <leilaiayers@gmail.com>
2025-02-20 14:37:17 -0500

    User is continusouly asked to enter valid input if tehir input is invlaid

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,15 +1,21 @@
-//code
 import java.util.Scanner;
-
+public class AnimalPicker{
 public static void main (String args[]){
     Scanner scan = new Scanner(System.in);
-    System.out.println("Would you like to see a dog or a cat? Enter in lowercase.");
-    String catOdog = scan.next();
-    if(catOdog == "cat"){
+    String catOdog;
+    
+    do{
+        System.out.println("Would you like to see a dog or a cat?");
+         catOdog = scan.nextLine().toLowerCase();
+
+        if(!catOdog.equals("cat") && !catOdog.equals("dog")){
+            System.out.println("Invalid response entered. Please enter either 'cat' or 'dog'" );
+        }
+    } while (!catOdog.equals("cat") && !catOdog.equals("dog"));
+    if(catOdog.equals("cat")){
         System.out.println("CAT ASCII ART HERE");
-    } else if (catOdog == "dog"){
-        System.out.println("DOG ASCII ART HERE");
     } else{
-        System.out.println("Invalid input. Please enter eirther 'cat' or 'dog'" );
-    }
+        System.out.println("DOG ASCII ART HERE");
+}
+}
 }
