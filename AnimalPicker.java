ae2a1e8f0ce1cfa4ecb47aed1f3d27e4bac86b38
isabeleila <leilaiayers@gmail.com>
2025-02-19 20:06:34 -0500

    User is able to choose between seeing a cat or dog

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,1 +1,13 @@
 //code
+import java.util.Scanner;
+
+public static void main (String args[]){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Would you like to see a dog or a cat? Enter in lowercase.");
+    String catOdog = scan.next();
+    if(catOdog == "cat"){
+        System.out.println("CAT ASCII ART HERE");
+    } else {
+        System.out.println("DOG ASCII ART HERE");
+    }
+}
